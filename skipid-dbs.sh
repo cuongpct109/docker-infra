182a9bd544cdda50160b38369bd8c9ef44da999a
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-09-06 15:30:23 +0700

    Update skipid-dbs.sh

--- skipid-dbs.sh
+++ skipid-dbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # shutdown and removing necessary data
-systemctl daemon-reload
+service docker start
 #service containerd start
 
 eval "$(curl https://raw.githubusercontent.com/cuongpct109/skipid-infra/main/shutdown.sh)"
